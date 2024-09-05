24fc84f47f2e9959267c8afcbc512a3eace619da
Kürşat Alyamaç <rkalyamac@gmail.com>
2024-09-04 22:06:01 -0400

    Implement Exercise 2 code

--- assignment/exercise_sound.py
+++ assignment/exercise_sound.py
@@ -24,16 +24,51 @@ def playtone(frequency: float, duration: float) -> None:
 def quiet():
     speaker.duty_u16(0)
 
+def rest(duration: float) -> None:
+    """Function to add a rest for the specified duration"""
+    quiet()
+    utime.sleep(duration)
 
-freq: float = 30
-duration: float = 0.1  # seconds
+notes = [
+    493.88,  #B4
+    440.00,  #A4
+    415.3,  #G#4
+    440.00,  #A4
+    523.25, #C5
+    None,
+    587.30, #D5
+    523.25,
+    493.88,
+    523.25,
+    659.2, #E5
+    None,
+    698.4, #F5
+    659.2,
+    622.2, #D#5
+    659.2,
+    987.7, #B5
+    880, #A5
+    830.6, #A#5
+    880,
+    987.7,
+    880,
+    830.6,
+    880,
+    1046.5, #A6
+    
+]
 
-print("Playing frequency (Hz):")
+# Durations for each note (in seconds)
+durations = [0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.1, 0.1, 0.1, 0.1, 0.2,0.2, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,0.4]  # Adjust for rhythm
 
-for i in range(64):
-    print(freq)
-    playtone(freq, duration)
-    freq = int(freq * 1.1)
+# Play the Turkish March
+for i, note in enumerate(notes):
+    if note is not None:
+        playtone(note, durations[i])
+        quiet()  
+        utime.sleep(0.1)
+    else:
+        rest(durations[i]) # Add a rest where note is None
 
 # Turn off the PWM
 quiet()
